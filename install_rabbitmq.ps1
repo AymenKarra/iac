d6765edb9162db8e61277d53dd63fb8291f1104d
AymenKarra <76628712+AymenKarra@users.noreply.github.com>
2022-07-01 11:01:05 +0100

    Update install_rabbitmq.ps1

--- install_rabbitmq.ps1
+++ install_rabbitmq.ps1
@@ -1,5 +1,7 @@
 #install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force ; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+#install erlang
+choco install erlang -y
 #install rabbitmq
 choco install rabbitmq -y
