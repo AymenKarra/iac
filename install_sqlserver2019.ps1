779356dc713067846d4c12f0c3aff2f93ba4adbb
AymenKarra <76628712+AymenKarra@users.noreply.github.com>
2022-07-14 16:43:14 +0100

    Update install_sqlserver2019.ps1

--- install_sqlserver2019.ps1
+++ install_sqlserver2019.ps1
@@ -2,4 +2,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force ; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 #install sql server 2019
-choco install sql-server-2019
+choco install sql-server-2019 -y
